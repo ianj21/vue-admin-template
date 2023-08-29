7c6c12f9adfc481f635ee70ae0465d08b187b8d7
ianj <ianj15@163.com>
2023-08-29 11:31:42 +0800

    clear

--- src/utils/debounce.ts
+++
@@ -1,8 +0,0 @@
-
-export const debounce = (fun: any, delay: number = 500) => (args?: any) => {
-  const _args = args;
-  clearTimeout(fun.id);
-  fun.id = setTimeout(() => {
-    fun.call(this, _args);
-  }, delay);
-};
